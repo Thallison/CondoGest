998687af52b36b77af4173bdbab4e9f5116a862e
Thallison Morais <t.morais@reply.com>
2022-07-10 15:15:12 -0300

    Configuranço Program.cs

--- src/Users/Program.cs
+++ src/Users/Program.cs
@@ -1,4 +1,9 @@
 using User.Helpers;
+using User.Services.Interfaces;
+using User.Services;
+using System.Text.Json.Serialization;
+using User.Authorization;
+using User.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -7,19 +12,54 @@ var services = builder.Services;
 services.AddDbContext<ApplicationDbContext>();
 
 services.AddControllers();
+
+services.AddTransient<ITokenService,TokenService>();
+
+services.AddCors();
+services.AddControllers().AddJsonOptions(x =>
+{
+    // serialize enums as strings in api responses (e.g. Role)
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+    // ignore omitted parameters on models to enable optional params (e.g. User update)
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
+services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+services.AddScoped<IJwtUtils, JwtUtils>();
+services.AddScoped<IUserService, UserService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+{
+    // global cors policy
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    // global error handler
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+
+    // custom jwt auth middleware
+    app.UseMiddleware<JwtMiddleware>();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
