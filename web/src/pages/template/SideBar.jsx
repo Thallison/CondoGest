fe0c6fda97b9925341b62206e1bd14693051843b
Wanderson <wandersonmg18@gmail.com>
2022-07-27 22:07:14 -0300

    Add modify

--- web/src/pages/template/SideBar.jsx
+++ web/src/pages/template/SideBar.jsx
@@ -5,7 +5,7 @@ import Menu from './Menu'
 export default props => (
     <aside className='main-sidebar sidebar-dark-primary'>
         <Link to={`${process.env.PUBLIC_URL}/app`} className="brand-link">
-            <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Condogest Logo" className="brand-image" />
+            <img src={`${process.env.PUBLIC_URL}/C_condogest.png`} alt="Condogest Logo" className="brand-image" />
             <span className="brand-text" style={{marginLeft: 1+'em'}}>Condogest</span>
         </Link >
         <section className="sidebar">
