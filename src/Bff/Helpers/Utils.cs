217cc91b8a812e476e2f8bbaf8edc47d9d595118
Thallison Morais <t.morais@reply.com>
2022-07-31 23:18:49 -0300

    Criando metodo delete Bff

---
+++ src/Bff/Helpers/Utils.cs
@@ -0,0 +1,15 @@
+namespace Bff.Helpers
+{
+    public class Utils
+    {
+        public static string formatCpf(string cpf)
+        {
+            return Convert.ToUInt64(cpf).ToString(@"000\.000\.000\-00");
+        }
+
+        public static string cleanCpf(String cpf)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(cpf, "[^a-z0-9_]+", "-");
+        }
+    }
+}
