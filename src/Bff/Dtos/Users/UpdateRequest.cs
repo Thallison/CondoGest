9645685593f9d509527f0c1bb088a71941da2a01
Thallison Morais <t.morais@reply.com>
2022-07-31 23:43:03 -0300

    Correção pronto 2

--- src/Bff/Dtos/Users/UpdateRequest.cs
+++ src/Bff/Dtos/Users/UpdateRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Bff.Helpers.Enums;
+using Bff.Helpers;
 
 namespace Bff.Dtos.Users
 {
@@ -14,8 +15,21 @@ namespace Bff.Dtos.Users
         [Required]
         public UserStatus Status { get; set; }
         
+        private string _cpf;
+        
         [Required]
-        public string Cpf { get; set; }
+        public string Cpf
+        {
+            get
+            {
+                return this._cpf;
+            }
+
+            set 
+            {
+                _cpf = Utils.cleanCpf(value);
+            }
+        }
         
         public string? Rg { get; set; }
         
