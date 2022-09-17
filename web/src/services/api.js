28ecbd67823a0c05a66c2cc712115631cabc6ee9
Thallison Morais <t.morais@reply.com>
2022-09-17 19:35:50 -0300

    Config Api

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "https://condogest-bff.herokuapp.com/"
+  baseURL: "http://host.docker.internal:49166/"
 });
 
 api.interceptors.request.use(async config => {
